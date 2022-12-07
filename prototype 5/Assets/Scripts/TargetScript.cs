040c0915672918a549b780802c31699425325e1f
Melek <nittoriyumalik@gmail.com>
2022-12-07 22:40:56 +0100

    initiate commit

---
+++ prototype 5/Assets/Scripts/TargetScript.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetScript : MonoBehaviour
+{
+    //public GameObject[] prefabs;
+    private Rigidbody targetRb;
+    private GameManager gameManager;
+
+    private float minSpeed = 60, maxSpeed = 70;
+    private float minTorque = -50, maxTorque = 50;
+    private float xRange = 5, ySpawn = -1;
+
+    public int pointValue;
+    public ParticleSystem particleExplosion;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetRb = GetComponent<Rigidbody>();
+        targetRb.AddForce(RandomForce(), ForceMode.Impulse);
+        targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
+
+        transform.position = RandomSpawnPos();
+
+        gameManager = GameObject.Find("GameManager").
+            GetComponent<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minSpeed, maxSpeed);
+    }
+
+    float RandomTorque()
+    {
+        return Random.Range(minTorque, maxTorque);
+    }
+
+    Vector3 RandomSpawnPos()
+    {
+        return new Vector3(Random.Range(-xRange, xRange), ySpawn);
+    }
+
+    private void OnMouseDown()
+    {
+        if (gameManager.isGameActive)
+        {
+            Destroy(gameObject);
+            Instantiate(particleExplosion, transform.position, particleExplosion.transform.rotation);
+            gameManager.UpdateScore(pointValue);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+        if (!gameObject.CompareTag("Bad"))
+        {
+            gameManager.GameOver();
+        }
+    }
+}
